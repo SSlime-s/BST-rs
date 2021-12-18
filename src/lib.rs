9ee219b80d67b885061440578c617ec56fc9033c
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2021-12-18 23:19:03 +0900

    ⚙️ bin から lib に

---
+++ src/lib.rs
@@ -0,0 +1,4 @@
+pub mod red_black_tree;
+// mod splay_tree;
+pub mod avl_tree;
+pub mod tree_trait;
--- src/main.rs
+++
@@ -1,8 +0,0 @@
-mod red_black_tree;
-// mod splay_tree;
-mod avl_tree;
-mod tree_trait;
-
-fn main() {
-    println!("Hello, world!");
-}
