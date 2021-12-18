97fc0aa3ef82f31414ce6e28746bec5e6cdbe475
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2021-12-18 23:20:36 +0900

    💥 tree_trait を map ように書き換えた
    map から set なら雑に書き換えられるしね

--- src/tree_trait.rs
+++ src/tree_trait.rs
@@ -1,13 +1,13 @@
-pub trait BinarySearchTree<T> {
-    fn insert(&mut self, key: T);
-    fn remove(&mut self, key: T);
-    fn search(&self, key: T) -> bool;
-    fn min(&self) -> Option<&T>;
-    fn max(&self) -> Option<&T>;
+pub trait BinarySearchTree<K, V> {
+    fn insert(&mut self, key: K, value: V) -> bool;
+    fn remove(&mut self, key: K) -> Option<V>;
+    fn search(&self, key: K) -> Option<V>;
+    fn min(&self) -> Option<(&K, &V)>;
+    fn max(&self) -> Option<(&K, &V)>;
     fn size(&self) -> usize;
     fn is_empty(&self) -> bool {
         self.size() == 0
     }
-    fn find_by_order(&self, order: usize) -> Option<&T>;
-    fn order_of_key(&self, key: T) -> Option<usize>;
+    fn find_by_order(&self, order: usize) -> Option<(&K, &V)>;
+    fn order_of_key(&self, key: K) -> Option<usize>;
 }
