690c791d532d07a1af6d016a233e6c2983fc8ecf
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2021-12-19 16:34:56 +0900

    💚 search のテスト追加

--- tests/avl_tree_test.rs
+++ tests/avl_tree_test.rs
@@ -106,3 +106,25 @@ fn remove_test_not_exists() {
         vec![(1, 1), (2, 2), (3, 3), (4, 4), (5, 5)]
     );
 }
+
+#[test]
+fn search_test() {
+    let mut tree = AVLTree::new();
+    tree.insert(1, 1);
+    tree.insert(2, 2);
+    tree.insert(3, 3);
+    tree.insert(4, 4);
+    tree.insert(5, 5);
+    assert_eq!(tree.search(&4), Some(&4));
+}
+
+#[test]
+fn search_test_not_exists() {
+    let mut tree = AVLTree::new();
+    tree.insert(1, 1);
+    tree.insert(2, 2);
+    tree.insert(3, 3);
+    tree.insert(4, 4);
+    tree.insert(5, 5);
+    assert_eq!(tree.search(&6), None);
+}
