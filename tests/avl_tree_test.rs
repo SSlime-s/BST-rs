a6f2be7b679c5c039eb5380f1e5296c4ea54c284
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2021-12-19 16:44:58 +0900

    💚 全消し時のテストを

--- tests/avl_tree_test.rs
+++ tests/avl_tree_test.rs
@@ -68,6 +68,22 @@ fn remove_test() {
 }
 
 #[test]
+fn remove_test_all() {
+    let mut tree = AVLTree::new();
+    tree.insert(1, 1);
+    tree.insert(2, 2);
+    tree.insert(3, 3);
+    tree.insert(4, 4);
+    tree.insert(5, 5);
+    assert_eq!(tree.remove(&1), Some(1));
+    assert_eq!(tree.remove(&2), Some(2));
+    assert_eq!(tree.remove(&3), Some(3));
+    assert_eq!(tree.remove(&4), Some(4));
+    assert_eq!(tree.remove(&5), Some(5));
+    assert!(tree.is_empty())
+}
+
+#[test]
 fn remove_test_confused() {
     let mut tree = AVLTree::new();
     tree.insert(4, 4);
