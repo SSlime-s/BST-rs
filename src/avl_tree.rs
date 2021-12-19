41ef64fea1e346b4fe2b2d660051fe5101d16217
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2021-12-19 15:44:02 +0900

    ♻️ 結局使わなかった関数消した

--- src/avl_tree.rs
+++ src/avl_tree.rs
@@ -53,10 +53,6 @@ impl<K, V> From<NodePtrInner<K, V>> for NodePtr<K, V> {
     }
 }
 impl<K, V> NodePtr<K, V> {
-    fn new(key: K, value: V) -> Self {
-        NodePtr(Some(Box::new(Node::new(key, value))))
-    }
-
     fn size(&self) -> usize {
         self.0.as_ref().map_or(0, |node| node.size)
     }
